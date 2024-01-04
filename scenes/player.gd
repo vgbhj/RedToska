2b32b05fd0a07db9c8b82216e241f8ed9a68fd79
VgbHj <mishaslsk@gmail.com>
2024-01-05 01:13:10 +0300

    Create jab_attack

--- scenes/player.gd
+++ scenes/player.gd
@@ -3,7 +3,9 @@ extends CharacterBody2D
 @export var speed: int = 350
 @onready var animations = $AnimatedSprite2D/AnimationPlayer
 @onready var collider = $CollisionShape2D
-var lastAnimDirection: String = "Right"
+@onready var jabAttackCollider = $AnimatedSprite2D/JabAttack/JabAttackCollider
+var lastAnimDirection: String = "Left"
+var isAttacking: bool = false
 
 func get_input():
 	velocity = Vector2.ZERO
@@ -17,23 +19,37 @@ func get_input():
 		velocity.y -= 1
 	
 	if Input.is_action_just_pressed("jab_attack"):
-		pass
+		print(lastAnimDirection)
+		if lastAnimDirection == "Right":
+			jabAttackCollider.position.x = $AnimatedSprite2D.position.x - 40
+		else:
+			jabAttackCollider.position.x = $AnimatedSprite2D.position.x + 10 
+		
+		animations.play("jab_attack")
+		isAttacking = true
+		await animations.animation_finished
+		isAttacking = false
 		
 	velocity = velocity.normalized() * speed
-	print(velocity)
 
 func updateAnimation():
-	var direction = "Right"
+	if isAttacking:
+		return
 	if velocity == Vector2.ZERO:
 		animations.play("idle")
 		collider.position.x = $AnimatedSprite2D.position.x - 10
 	else:
 		animations.play("walk")
 		$AnimatedSprite2D.flip_h = velocity.x < 0
+		
+		var direction = "Left"
+		if velocity.x < 0:
+			direction = "Right"
+			
+		lastAnimDirection = direction
 	
 	collider.position.x = $AnimatedSprite2D.position.x
-	
-	lastAnimDirection = direction
+
 
 func _physics_process(delta):
 	get_input()
