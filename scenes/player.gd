087d42908eca83dc1af032ebf5137f19f578878f
VgbHj <mishaslsk@gmail.com>
2024-01-04 20:39:46 +0300

    Add AnimationPlayer

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,25 +1,36 @@
 extends CharacterBody2D
 
 @export var speed: int = 350
-@onready var animations = $AnimatedSprite2D
+@onready var animations = $AnimatedSprite2D/AnimationPlayer
 @onready var collider = $CollisionShape2D
+var lastAnimDirection: String = "Right"
 
 func handleInput():
 	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = moveDirection*speed
+	
+	if Input.is_action_just_pressed("jab_attack"):
+		pass
 
 func updateAnimation():
 	var direction = "Right"
 	if velocity == Vector2.ZERO:
 		animations.play("idle")
-		collider.position.x = animations.position.x - 10
+		collider.position.x = $AnimatedSprite2D.position.x - 10
 	else:
 		animations.play("walk")
-		animations.flip_h = velocity.x < 0
+		$AnimatedSprite2D.flip_h = velocity.x < 0
 	
-	collider.position.x = animations.position.x
+	collider.position.x = $AnimatedSprite2D.position.x
+	
+	lastAnimDirection = direction
 
 func _physics_process(delta):
 	handleInput()
 	move_and_slide()
 	updateAnimation()
+
+
+func _on_jab_attack_area_entered(area):
+	if area.is_in.group("hurtbox"):
+		area.take_damage()
